856c544d28d48c302f841b3c4eee7a3bfa09cbb3
Wholemeal Gaunt <wgaunt@student.21-school.ru>
2022-06-23 17:06:11 +0300

    Rewrite reading/writing (now work with structure) + add Create method

--- Test/test.cpp
+++ Test/test.cpp
@@ -1,11 +1,40 @@
-#include <string>
+#include <iostream>
 #include "VFS.hpp"
 
-int main( int argc, char **argv )
+std::ostream &operator<<( std::ostream& s, TestTask::File &f )
+{
+	s << "Address: " << f.addr << std::endl \
+	  << "Pointer: " << f.p << std::endl \
+	  << "Name:    " << f.name << std::endl \
+	  << "Next:    " << f.content.next << std::endl \
+	  << "Mod:     " << (int)f.content.mod << std::endl \
+	  << "Extra:   " << f.content.addr_extra;
+	return (s);
+}
+
+std::ostream &operator<<( std::ostream& s, TestTask::Content &c )
+{
+	s << std::hex \
+	<< "Next:    " << c.next << std::endl \
+	<< "Mod:     " << (int)c.mod << std::endl \
+	<< "Extra:   " << c.addr_extra;
+	return (s);
+}
+
+int main()
 {
 	TestTask::File *vfs_file;
 	TestTask::VFS vfs;
 
 	vfs_file = vfs.Create("My_Top_File");
+	std::cout << *vfs_file << std::endl;
+
+	std::cout << "-----------------" << std::endl;
+
+	vfs_file = vfs.Open("My_Top_File");
+	if (vfs_file)
+		std::cout << *vfs_file << std::endl;
+	else
+		std::cout << "Error: File not found!" << std::endl;
 	return (0);
 }
